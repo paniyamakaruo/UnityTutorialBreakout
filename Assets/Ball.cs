5f61d8330fa0a634e61487dcc1fcee43a2db8a08
paniyamakaruo <rokusyou27@gmail.com>
2018-10-06 19:57:16 +0900

    初期化

---
+++ Assets/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private float speed = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Rigidbody>().AddForce(
+            (transform.forward + transform.right) * speed,
+            ForceMode.VelocityChange);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
